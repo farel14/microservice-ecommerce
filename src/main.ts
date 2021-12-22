438323c6f006e78420a8fdaeccd72541d523b7d5
farel14 <72905415+farel14@users.noreply.github.com>
2021-12-22 08:23:41 +0700

    feat:init user

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-    },
-  );
-  app.listen();
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.TCP,
+  //   },
+  // );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  await app.listen(3000);
 }
 bootstrap();
